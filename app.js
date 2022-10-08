acc4948610cc501c2db2f8ceafda67686e29483e
Ekete Emmanuel <eketeeu@gmail.com>
2022-10-08 04:18:53 -0700

    enabled cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const app = express();
 const path = require ('path');
 require('dotenv').config();
+const cors = require('cors') //Cross-origin Resource Sharing
 const PORT = process.env.PORT || 3000;;
 
 // Middlewares
@@ -9,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("/public")); // to render static file like CSS, plain javascript, fonts, images.
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());  // to initialise the cors middleware
 
 // router middlewares
 const indexRoute = require('./src/routes/index');
--- public/public/js/main.js
+++ public/public/js/main.js
@@ -1,3 +1,19 @@
+const toggleMenu = document.getElementById('menus');
+const toggleLinks = document.getElementById('link');
+const toggleClose = document.getElementById('close');
+
+if(toggleMenu){
+    toggleMenu.addEventListener('click', () => {
+        toggleLinks.classList.add('active')
+        // console.log("rad")
+    })
+}
+
+if(toggleClose){
+    toggleClose.addEventListener('click', () => {
+        toggleLinks.classList.remove('active')
+    })
+}
 
 //////register api////
 
